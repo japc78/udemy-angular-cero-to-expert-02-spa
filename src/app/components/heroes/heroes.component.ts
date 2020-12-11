5e084822d7fad6187c4c4bc0d3ec954d4c141ca6
japc <japc.grafico@gmail.com>
2020-12-11 13:04:56 +0100

    Page & Service of Heroes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Hero } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styles: [
-  ]
+  templateUrl: './heroes.component.html'
 })
+
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[] = [];
+
+  constructor( private _heroesService: HeroesService ) {}
 
   ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
   }
-
 }
